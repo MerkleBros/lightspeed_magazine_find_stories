4ff55b68af3eb846ff285b3f18f0bf1833f66f03
Patrick McCarver <patrick@merklebros.com>
2020-01-18 01:52:00 -0500

    Request, find, and save story data from story urls
    Build a dictionary for every story in the magazine, then save a
    dictionary of stories as a pickle. Keep track of urls where we failed to
    get the story data in a pickle named 'failed_story_urls.p'.
    
    Each story has a dictionary with the structure:
        { "author": "author name"
        , "title": "story title"
        , "story_url": "magazine.com/story1234"
        , "issue": "story issue name"
        , "issue_url": 'magazine.com/issue1234'
        , "word_count": "1234"
        , "type": "Fiction"
        , "content": "body of story" }
    
    The dictionary of stories has string keys of the form:
    "story title-author name"
    Example key: "The Traditional-Maria Dahvana Headley"
    
    The dictionary of stories has values that are story dictionaries as
    described above.
    
    New functions:
    
    Find all story information from a story's html page (Beautiful Soup
    object) and return a dictionary representing that story.
    find_story_from_story_page()
    
    Request all story pages using a list of urls, then find each story's
    data, build the story's dictionary, add that story to the stories
    dictionary, and save the stories dictionary to a pickle file.
    Also keep track of urls where story data could not be scraped in
    'failed_story_urls.p'.
    request_and_find_and_save_stories_from_story_links()

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 """Functions for requesting and processing html from Science Fiction magazines"""
 import os
+import re
 import time
 import pickle
+import traceback
 import requests
 from bs4 import BeautifulSoup
 
@@ -149,21 +151,152 @@ def request_and_find_and_save_story_links_from_issues(issue_links: list, pickle_
 
         time.sleep(5)
 
+def find_story_from_story_page(story_page: object):
+    """Return a dictionary representing a story from a story html page"""
+    print(f"Finding story from story page")
+
+    story = {}
+
+    content_box = story_page.body \
+        .find("div", {"id": "wrapper"}) \
+        .find("div", {"id": "main"}) \
+        .find("div", {"id": "content"}) \
+        .find("div", {"class": "content_box"}) \
+
+    story_type = content_box.find("h3").contents[0]
+
+    author = content_box \
+        .find("div", {"class": "about_author"}) \
+        .find("h2") \
+        .find("span") \
+        .contents[0]
+
+    post_div = content_box \
+        .find("div", {"id": re.compile("^post-")})
+
+    title = post_div \
+        .find("h1", {"class": "posttitle"}) \
+        .contents[0]
+
+    issue_paragraph = post_div \
+        .find("p", {"class": re.compile("postmetadata date")})
+
+    issue = issue_paragraph.find("a").contents[0]
+    issue_url = issue_paragraph.find("a")["href"]
+    word_count = re.search(r'\b\d+\b', issue_paragraph.contents[2]).group(0)
+
+    story_div = post_div \
+        .find("div", {"class": "entry"})
+
+    story_div_elements = list(story_div.children)
+
+    content = ""
+    for element in story_div_elements:
+        # ignore paragraphs containing images or links
+        if element.find("img") or element.find("a"):
+            continue
+        if element.name in ["p"]:
+            content += f"{element.get_text()}\n\n"
+        if element.name in ["ol", "ul"]:
+            text = element.get_text('\n')
+            content += f"{text}"
+        if element.name == "div" and element["class"] == "divider":
+            content += f". . . .\n\n"
+
+    story.update(
+        { "author": str(author)
+        , "title" : str(title)
+        , "issue": str(issue)
+        , "issue_url": str(issue_url)
+        , "word_count": str(word_count)
+        , "type": str(story_type)
+        , "content": content
+        })
+
+    return story
+
+def request_and_find_and_save_stories_from_story_links(story_links: list, pickle_url: str):
+
+    """
+    Request, find, and save all stories and story metadata as a pickle
+
+    Builds a dictionary of stories and saves it as a pickle.
+
+    stories dictionary structure:
+    keys: "story title-Author Name"
+    example key: "The Traditional-Maria Dahvana Headley"
+    values: a story dictionary containing story content and metadata
+      { "author": "author name"
+      , "title": "story title"
+      , "story_url": "magazine.com/story1234"
+      , "issue": "story issue name"
+      , "issue_url": 'magazine.com/issue1234'
+      , "word_count": "1234"
+      , "type": "Fiction"
+      , "content": "body of story"
+      }
+    """
+
+    print(f"Getting all stories from story links")
+
+    stories = {}
+    failed_story_urls = []
+    for story_link in story_links:
+        try:
+            if os.path.exists(pickle_url):
+                stories = load_from_pickle(pickle_url)
+                print(f"Loaded pickle {pickle_url} with {len(stories)} stories")
+                story_urls = [story["story_url"] for story in list(stories.values())]
+                if story_link in story_urls:
+                    print(f"Found story in pickle, skipping story {story_link}")
+                    continue
+
+            story_page = request_soup_page(url=story_link)
+            story = find_story_from_story_page(story_page=story_page)
+            story["story_url"] = story_link
+
+            if not all(keys in story for keys in \
+                        ("author", "title", "issue", "issue_url", "word_count", "content")):
+                print(f"Problem getting story data from story url {story_link}")
+                raise Exception("Malformed story data")
+
+            stories[f'{story["title"]}-{story["author"]}'] = story
+
+            print(f"Found {len(stories)} stories so far")
+            print(f"Dumping {len(stories)} stories to pickle {pickle_url}")
+
+            with open(pickle_url, 'wb') as file_pointer:
+                pickle.dump(stories, file_pointer)
+
+            time.sleep(5)
+
+        except Exception as err:
+            print(f"Failed to retrieve and process story {story_link}, adding to failed_story_urls.p")
+            print(traceback.format_exc())
+            failed_story_urls.append(story_link)
+
+            with open('failed_story_urls.p', 'wb') as file_pointer:
+                pickle.dump(failed_story_urls, file_pointer)
+
 def main():
     """
     Run functions to retrieve and process html from online science fiction magazines.
 
     Only need to run each once to save data to pickle files.
 
-    Two pickle files are saved:
+    Three pickle files are saved:
     issue_links.p is a list of url strings for all magazine issues
     story_links.p is a list of url strings for all stories in the magazine
+    stories.p is an object containing story content and metadata for all stories
     """
 
     request_and_find_and_save_issue_links()
     issue_links = load_from_pickle("issue_links.p")
     request_and_find_and_save_story_links_from_issues( \
         issue_links=issue_links, pickle_url="story_links.p")
+    story_links = load_from_pickle(url="story_links.p")
+    request_and_find_and_save_stories_from_story_links( \
+        story_links=story_links, pickle_url="stories.p")
 
 if __name__ == '__main__':
     main()
